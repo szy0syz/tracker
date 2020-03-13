288e06f9f130c1426134dfc1f265a111145de19b
szy0syz <szy0syz@gmail.com>
2020-03-13 22:45:03 +0800

    Init Project

--- components/Layouts.js
+++ components/Layouts.js
@@ -1,15 +1,4 @@
-# tracker
-
-> a next.js demo
-
-## Notes
-
-### 关于 'next/head'
-
-- 注意啊，这里 `<Head>` 处理的是 `html` 文档的 `head` 标签内容
-
-```js
-import Head from 'next/head'
+import Head from 'next/head';
 
 const Layouts = ({ children }) => {
   return (
@@ -22,4 +11,5 @@ const Layouts = ({ children }) => {
     </div>
   );
 };
-```
+
+export default Layouts;
---
+++ pages/About.js
@@ -0,0 +1,11 @@
+import Layouts from '../components/Layouts'
+
+const About = () => {
+  return (
+    <Layouts>
+      <h1>About22</h1>
+    </Layouts>
+  );
+};
+
+export default About;
